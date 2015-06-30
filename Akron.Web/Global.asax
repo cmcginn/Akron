72d76f36a7dfa00f23e6b6d613696105200668bf
csmfdecisons <fdecisons2015>
2015-06-30 16:52:40 -0400

    Initial Project Creation

---
+++ Akron.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Akron.Web.MvcApplication" Language="C#" %>
---
+++ Akron.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Akron.Web.Startup))]
+namespace Akron.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
